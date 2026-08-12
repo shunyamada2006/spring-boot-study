6ddcfd4c98f1ec2cf840ca0384ee69745839eddd
Shun Yamada <shantianshun42@gmail.com>
2026-08-13 03:50:11 +0900

    プロジェクトの初期設定を学習

---
+++ src/main/java/com/example/spring_tutorial/controller/FirstController.java
@@ -0,0 +1,11 @@
+package com.example.spring_tutorial.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+
+public class FirstController {
+    @GetMapping("/")
+    public String first(){
+        return "firstview";
+    }
+}
