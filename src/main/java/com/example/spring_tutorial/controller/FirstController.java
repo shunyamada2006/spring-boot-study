1f6c92d7f91851369c72dd2e00c88cf84bc7bef8
Shun Yamada <shantianshun42@gmail.com>
2026-08-14 13:53:34 +0900

    RestControllerの基本を学習

--- src/main/java/com/example/spring_tutorial/controller/FirstController.java
+++ src/main/java/com/example/spring_tutorial/controller/FirstController.java
@@ -1,11 +1,11 @@
 package com.example.spring_tutorial.controller;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-@Controller
+@RestController
 
 public class FirstController {
     @GetMapping("/")
     public String first(){
-        return "firstview";
+        return "これはRestコントローラーからのメッセージです";
     }
 }
