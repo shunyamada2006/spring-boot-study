ac0643655a0d98111e0b3b9f44c488b55c8f1a07
Shun Yamada <shantianshun42@gmail.com>
2026-08-16 19:53:56 +0900

    フォームクラスを学習

--- src/main/java/com/example/spring_tutorial/controller/AdminUserController.java
+++ src/main/java/com/example/spring_tutorial/controller/AdminUserController.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.spring_tutorial.entity.User;
+import com.example.spring_tutorial.form.UserRegisterForm;
 import com.example.spring_tutorial.service.UserService;
 
 @Controller
@@ -28,18 +28,22 @@ public class AdminUserController {
         // ビューにユーザーリストを渡す
         model.addAttribute("users", users);
 
+        if(!model.containsAttribute("userRegisterForm")){
+            model.addAttribute("userRegisterForm",new UserRegisterForm());
+        }
         return "adminUserView";
     }
 
     @PostMapping("/register")
     public String registerUser(RedirectAttributes redirectAttributes,
-                               @RequestParam("user_name") String userName,
-                               @RequestParam("password") String password,
-                               @RequestParam("role_id") int roleId) {
+            UserRegisterForm form){
+//                               @RequestParam("user_name") String userName,
+//                               @RequestParam("password") String password,
+//                               @RequestParam("role_id") int roleId) {
 
         try {
-            // リクエストパラメータからのデータを用いてユーザー登録
-            userService.createUser(userName, password, roleId);
+            userService.createUser(form.getUserName(), form.getPassword(), form.getRoleId());
+//            userService.createUser(userName, password, roleId);
 
             // 登録成功時は完了メッセージをビューに受け渡す
             redirectAttributes.addFlashAttribute("successMessage", "ユーザー登録が完了しました。");
@@ -47,10 +51,10 @@ public class AdminUserController {
         } catch (IllegalArgumentException e) {
             // 登録失敗時はエラーメッセージをビューに受け渡す
             redirectAttributes.addFlashAttribute("failureMessage", e.getMessage());
+            redirectAttributes.addFlashAttribute("userRegisterForm",form);
 
-            // 再表示用の入力データをビューに受け渡す（パスワードは除く）
-            redirectAttributes.addFlashAttribute("userName", userName);
-            redirectAttributes.addFlashAttribute("roleId", roleId);
+//            redirectAttributes.addFlashAttribute("userName", userName);
+//            redirectAttributes.addFlashAttribute("roleId", roleId);
         }
 
         // adminuserにリダイレクトしてリストを再表示
